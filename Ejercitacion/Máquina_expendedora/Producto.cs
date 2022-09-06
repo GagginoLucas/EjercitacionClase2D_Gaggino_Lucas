40bd947f343bac0f281c45ada4702806770102d5
GagginoLucas <gaggino17@gmail.com>
2022-09-05 22:30:10 -0300

    Entrega ejercicio Coleccion

---
+++ Ejercitacion/Máquina_expendedora/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Máquina_expendedora
+{
+    internal class Producto
+    {
+        private int precio;
+        private string nombre;
+
+        public Producto(int precio, string nombre)
+        {
+            this.precio = precio;
+            this.nombre = nombre;
+        }
+
+
+        public int Precio
+        {
+            get { return precio; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+        }
+
+    }
+
+     
+}
---
+++ Ejercitacion/Máquina_expendedora/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * 1.Crear e instanciar el Dictionary<TKey,TValue> llamado maquinaExpendedora.
+2.Cargar por lo menos 10 elementos en formato clave-valor (clave=numero de posición, valor=nombre del producto)
+3.Una vez cargado, desarrollar el flujo del proyecto de consola de modo que:
+•Al iniciarse muestre al usuario todos los productos con sus respectivos códigos.
+•Pida al usuario elegir el número del producto que quiere
+•Al seleccionar número, informar al usuario que recibió ese producto y volver a mostrar todos los productos SIN el producto que ya eligió
+•El programa se repetirá indefinidamente hasta que el usuario elija la opción “S”.
+BONUS: Refactorizar el programa para que en vez de tener un diccionario INT – STRING, tengamos un diccionario INT – PRODUCTO (que contenga nombre y precio del producto)
+ * */
+
+
+using Biblioteca;
+using System;
+using System.Collections.Generic;
+
+namespace Máquina_expendedora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, Producto> maquinaExpendedora = new Dictionary<int, Producto>();
+            int opcionElegida;
+            string salir;
+
+            maquinaExpendedora.Add(1, new Producto(250, "Cocucha"));
+            maquinaExpendedora.Add(2, new Producto(250, "Papitas"));
+            maquinaExpendedora.Add(3, new Producto(250, "Alfajorsito"));
+            maquinaExpendedora.Add(4, new Producto(250, "Birrita"));
+            maquinaExpendedora.Add(5, new Producto(250, "Chipasitos"));
+            maquinaExpendedora.Add(6, new Producto(250, "Fernet de litro"));
+            maquinaExpendedora.Add(7, new Producto(250, "Chocolate"));
+            maquinaExpendedora.Add(8, new Producto(250, "Felicidad"));
+            maquinaExpendedora.Add(9, new Producto(250, "Clonazepam"));
+            maquinaExpendedora.Add(10, new Producto(250, "Frapanui"));
+
+            do
+            {
+                foreach (KeyValuePair<int, Producto> item in maquinaExpendedora)
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value.Nombre} - Precio: {item.Value.Precio} $");
+                }
+                opcionElegida = Metodos.PedirEnteroEnRango("Seleccione un producto de la máquina expendedora", 1, 10);
+
+                Console.WriteLine($"Elegiste {maquinaExpendedora[opcionElegida].Nombre} - Precio: {maquinaExpendedora[opcionElegida].Precio}");
+
+                maquinaExpendedora.Remove(opcionElegida);
+
+                foreach (KeyValuePair<int, Producto> item in maquinaExpendedora)
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value.Nombre} - Precio: {item.Value.Precio} $");
+                }
+                Console.WriteLine("Si desea abandonar la máquina expendedora presione 'S'");
+                salir = Console.ReadLine();
+                
+            } while (salir!= "S");
+      
+        }
+    }
+}
